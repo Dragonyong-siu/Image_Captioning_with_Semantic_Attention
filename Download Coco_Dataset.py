ef1076d0caa8b85df6cb02f6f58fad1ed1a639f7
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-09-09 19:08:37 +0900

    Update Download Coco_Dataset.py

--- Download Coco_Dataset.py
+++ Download Coco_Dataset.py
@@ -1,2 +1,11 @@
 !wget http://images.cocodataset.org/zips/val2014.zip
 !unzip val2014.zip
+
+import torchvision
+from torchvision import transforms
+Data_Transforms = torchvision.transforms.Compose([transforms.Resize((256, 256))])
+Train_data = torchvision.datasets.CocoCaptions(root = 'val2014/',
+                                               annFile = '/content/gdrive/My Drive/captions_val2014.json',
+                                               transform = Data_Transforms,
+                                               target_transform = None,
+                                               transforms = None)
