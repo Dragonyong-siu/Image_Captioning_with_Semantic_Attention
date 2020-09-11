c13a03f9c5f33e5b778974347bbc2da9693439ad
Dragonyong-siu <68500343+Dragonyong-siu@users.noreply.github.com>
2020-09-11 16:24:10 +0900

    Update Semantic_Caption_Model_GPT2.py

--- Semantic_Caption_Model_GPT2.py
+++ Semantic_Caption_Model_GPT2.py
@@ -12,54 +12,48 @@ class Semantic_Caption_Model_GPT2(nn.Module):
 
     self.GPT2_Hidden = 768
     self.GPT2_layers = 12
-    self.Model_insize = 768
-    self.Model_outsize = 768
-    self.Model_lmsize = 768
-    self.Sequence_Length = 15
+    self.Sequence_Length = 30
     self.Vocab_size = 50260
     
     self.ReLU = nn.ReLU(inplace = True)
-    self.Dropout = nn.Dropout(0.01, inplace = True)
+    self.Dropout = nn.Dropout(0.001, inplace = True)
     
     self.Linear_FC = nn.Linear(8 * 8 * 512, self.GPT2_Hidden)
-    self.Linear_HH = nn.Linear(8 * 8 * 512, self.Model_insize)
-    self.Linear_CC = nn.Linear(8 * 8 * 512, self.Model_insize)
-    self.Linear_LM = nn.Linear(self.Model_lmsize, self.Vocab_size)
+    self.Linear_HH = nn.Linear(8 * 8 * 512, self.GPT2_Hidden)
+    self.Linear_CC = nn.Linear(8 * 8 * 512, self.GPT2_Hidden)
+    self.Linear_LM = nn.Linear(self.GPT2_Hidden, self.Vocab_size)
     
-    self.Linear_IAW = nn.Linear(self.GPT2_Hidden, self.Model_insize)
     self.Linear_IAU = nn.Linear(self.GPT2_Hidden, self.GPT2_Hidden)
     self.Linear_Indiag = nn.Linear(self.GPT2_Hidden, self.GPT2_Hidden)
-    self.Linear_Indomain = nn.Linear(self.GPT2_Hidden, self.Model_insize)
-    self.Linear_OAV = nn.Linear(self.Model_outsize, self.GPT2_Hidden)
-    self.Linear_Outdiag = nn.Linear(self.GPT2_Hidden, self.Model_outsize)
-    self.Linear_Outdomain = nn.Linear(self.Model_outsize, self.Model_lmsize)
+    self.Linear_BAU = nn.Linear(self.GPT2_Hidden, self.GPT2_Hidden)
+    self.Linear_Bodydiag = nn.Linear(self.GPT2_Hidden, self.GPT2_Hidden)
+    self.Linear_OAV = nn.Linear(self.GPT2_Hidden, self.GPT2_Hidden)
+    self.Linear_Outdiag = nn.Linear(self.GPT2_Hidden, self.GPT2_Hidden)
 
   def forward(self, caption_ids, position_ids, feature_map, attributes):
     feature_map = feature_map.view(-1, 8 * 8 * 512)
     first_inputs = self.Linear_FC(feature_map) 
-    first_inputs = self.ReLU(first_inputs)
     input_wte = self.GPT2_wte(caption_ids)[:, :-1]
     input_wpe = self.GPT2_wpe(position_ids)
     
-    model_inputs_init = self.Input_Attention_Model_init(first_inputs)
-    model_inputs = self.Input_Attention_Model(input_wte, attributes)
-    input_Embedding = torch.cat([model_inputs_init.unsqueeze(1), model_inputs], dim = 1)
-    input_Embedding = input_Embedding + input_wpe
-    ## input_Embedding = self.GPT2_drop(input_Embedding)
+    first_inputs = first_inputs.unsqueeze(1)
+    Orig_Embedding = torch.cat([first_inputs, input_wte], dim = 1)
+    Attr_Embedding = self.Head_Attention_Model(Orig_Embedding, attributes)
+    input_Embedding = Orig_Embedding + Attr_Embedding + input_wpe
     
     for i in range(self.GPT2_layers):
       input_Embedding = self.GPT2_h[i](input_Embedding)[0]
+      input_Embedding = self.Body_Attention_Model(input_Embedding, attributes)
 
-    Hidden_states = self.GPT2_ln_f(input_Embedding)
-    Logits = self.Output_Attention_Model(Hidden_states, attributes)
+    Orig_Logits = self.GPT2_ln_f(input_Embedding)
+    Attr_Logits = self.Foot_Attention_Model(Orig_Logits, attributes)
+
+    Logits = Orig_Logits + Attr_Logits
+    Logits = self.Linear_LM(Logits)
     Logits = self.Dropout(Logits)
     return Logits
-    
-  def Input_Attention_Model_init(self, image_v):
-    model_inputs = self.Linear_IAW(image_v)
-    return model_inputs
   
-  def Input_Attention_Model(self, before_words, attributes):
+  def Head_Attention_Model(self, before_words, attributes):
     attributes = self.GPT2_wte(attributes).transpose(2, 1)
     Matmul1 = self.Linear_IAU(before_words)
     Matmul2 = torch.matmul(Matmul1, attributes)
@@ -67,10 +61,19 @@ class Semantic_Caption_Model_GPT2(nn.Module):
     
     Matmul3 = torch.matmul(attributes_weight_a, attributes.transpose(1, 2))
     model_inputs = before_words + self.Linear_Indiag(Matmul3)
-    model_inputs = self.Linear_Indomain(model_inputs)
     return model_inputs
 
-  def Output_Attention_Model(self, hidden_states, attributes):
+  def Body_Attention_Model(self, body_states, attributes):
+    attributes = self.GPT2_wte(attributes).transpose(2, 1)
+    Matmul1 = self.Linear_BAU(body_states)
+    Matmul2 = torch.matmul(Matmul1, attributes)
+    attributes_weight_a = F.softmax(Matmul2, dim = 2)
+    
+    Matmul3 = torch.matmul(attributes_weight_a, attributes.transpose(1, 2))
+    model_inputs = body_states + self.Linear_Bodydiag(Matmul3)
+    return model_inputs
+
+  def Foot_Attention_Model(self, hidden_states, attributes):
     attributes = self.GPT2_wte(attributes).transpose(2, 1)
     Matmul1 = self.Linear_OAV(hidden_states)
     Matmul2 = torch.matmul(Matmul1,attributes)
@@ -78,16 +81,11 @@ class Semantic_Caption_Model_GPT2(nn.Module):
 
     Matmul3 = torch.matmul(attributes_weight_b, attributes.transpose(1, 2))
     model_outputs = hidden_states + self.Linear_Outdiag(Matmul3)
-    model_outputs = self.Linear_Outdomain(model_outputs)
-    model_outputs = self.ReLU(model_outputs)
-    model_outputs = self.Linear_LM(model_outputs)
     return model_outputs
   
   def Semantic_Caption_Sampling(self, feature_map, attributes, max_len):
     feature_map = feature_map.view(-1, 8 * 8 * 512)
     feature_inputs = self.Linear_FC(feature_map) 
-    feature_inputs = self.ReLU(feature_inputs)
-    feature_inputs = self.Input_Attention_Model_init(feature_inputs) 
     feature_inputs = feature_inputs.unsqueeze(1)
     
     Sample_Ids = []
@@ -97,23 +95,26 @@ class Semantic_Caption_Model_GPT2(nn.Module):
       Addition_inputs = Addition_inputs.to(device)
       Addition_inputs = self.GPT2_wte(Addition_inputs)
       Addition_inputs = Addition_inputs.unsqueeze(0) 
-      Addition_inputs = self.Input_Attention_Model(Addition_inputs, attributes)
       
-      Sampling_inputs = torch.cat((feature_inputs, Addition_inputs), dim = 1)
-      inputs_length = Sampling_inputs.size(1)
+      Orig_inputs = torch.cat((feature_inputs, Addition_inputs), dim = 1)
+      Attr_inputs = self.Head_Attention_Model(Orig_inputs, attributes)
       
+      inputs_length = Orig_inputs.size(1)
       Position_inputs = torch.Tensor(np.arange(inputs_length))
       Position_inputs = Position_inputs.long()
       Position_inputs = Position_inputs.to(device)
       Position_inputs = self.GPT2_wpe(Position_inputs) 
-      Model_inputs = Sampling_inputs + Position_inputs
-      ## Model_inputs = self.GPT2_drop(Model_inputs)
+      Model_inputs = Orig_inputs + Attr_inputs + Position_inputs
       
       for j in range(self.GPT2_layers):
         Model_inputs = self.GPT2_h[j](Model_inputs)[0] 
+        Model_inputs = self.Body_Attention_Model(Model_inputs, attributes)
 
-      hidden_states = self.GPT2_ln_f(Model_inputs)
-      Sampling_outputs = self.Output_Attention_Model(hidden_states, attributes)
+      Orig_Logits = self.GPT2_ln_f(Model_inputs)
+      Attr_Logits = self.Foot_Attention_Model(Orig_Logits, attributes)
+
+      Logits = Orig_Logits + Attr_Logits
+      Sampling_outputs = self.Linear_LM(Logits)
 
       Words_Index = self.Next_Word_Index(Sampling_outputs[:, -1, :])
       Words_Index = torch.Tensor([Words_Index])
@@ -139,7 +140,7 @@ class Semantic_Caption_Model_GPT2(nn.Module):
     Index_List = [(First_Index, First_value),
                   (Second_Index, Second_value),
                   (Third_Index, Third_value)]
-
+    print(Index_List)
     Index_Filtered = []
     for i in range(len(Index_List)):
       if Index_List[i][1] >= 0.35:
